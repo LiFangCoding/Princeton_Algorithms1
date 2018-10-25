8de891b0d51b8772b439b9d1974a1ac4e567be6b
LiFangCoding <fanglihust@gmail.com>
2018-10-25 22:29:14 +0900

    score 100/100

--- Hw1_percolation/PercolationStats.java
+++ Hw1_percolation/PercolationStats.java
@@ -15,8 +15,7 @@ public class PercolationStats {
     private double mean;
     private double stddev;
     private double sqrOfT;
-
-
+    
     public PercolationStats(int n, int trials) {
         if (n <= 0 || trials <= 0) {
             throw new IllegalArgumentException();
