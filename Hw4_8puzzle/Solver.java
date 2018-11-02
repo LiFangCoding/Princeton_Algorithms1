7d1d7158c213d2f6a3a30c772500f9ffa6717a79
LiFangCoding <fanglihust@gmail.com>
2018-11-02 18:57:27 +0900

    add double pq

--- Hw4_8puzzle/Solver.java
+++ Hw4_8puzzle/Solver.java
@@ -12,75 +12,89 @@ import java.util.Queue;
 
 public class Solver {
     private int moves;
-    private Queue<Board> queueOriginal;
-    private Queue<Board> queueTwin;
-    private Queue<Board> queueSol;
+    private boolean solvable;
 
-    private MinPQ<Board> minpq = new MinPQ<Board>(new Comparator<Board>() {
-        @Override
-        public int compare(Board o1, Board o2) {
-            Integer o1Priority = o1.dimension() + o1.manhattan();
-            Integer o2Priority = o2.dimension() + o2.manhattan();
+    private Queue<Board> sol;
+    private Queue<Board> solTwin;
 
-            return o1Priority.compareTo(o2Priority);
-        }
-    });
 
-    private MinPQ<Board> minpqTwin = new MinPQ<Board>(new Comparator<Board>() {
-        @Override
-        public int compare(Board o1, Board o2) {
-            Integer o1Priority = o1.dimension() + o1.manhattan();
-            Integer o2Priority = o2.dimension() + o2.manhattan();
-
-            return o1Priority.compareTo(o2Priority);
-        }
-    });
 
     public Solver(Board board) {
         moves = 0;
-        Board predecessor = null;
-        queueOriginal = new LinkedList<>();
 
+        Board predecessor = null;
+        sol = new LinkedList<>();
+        MinPQ<Board> minpq = new MinPQ<Board>(new comparatorBoard());
         minpq.insert(board);
 
-        while (!minpq.isEmpty()) {
-            predecessor = minpq.delMin();
-            queueOriginal.add(predecessor);
-            if (predecessor.isGoal()) {
+        Board predecessorTwin = null;
+        solTwin = new LinkedList<>();
+        MinPQ<Board> minpqTwin = new MinPQ<Board>( new comparatorBoard());
+        minpqTwin.insert(board.twin());
+
+        while (!minpq.isEmpty() && !minpqTwin.isEmpty()) {
+            if (searchOneStep(minpq, sol, predecessor)) {
+                solvable = true;
                 break;
             }
 
-            for (Board neighbor : predecessor.neighbors()) {
-                if (neighbor.equals(predecessor)) {
-                    continue;
-                }
-                minpq.insert(neighbor);
+            if (searchOneStep(minpqTwin, solTwin, predecessorTwin)) {
+                solvable = false;
+                break;
             }
             moves++;
         }
     }
 
+    private boolean searchOneStep(MinPQ<Board> pq, Queue<Board> sol, Board predecessor) {
+        Board b = pq.delMin();
+        sol.add(b);
 
+        if (b.isGoal()) {
+            return true;
+        }
 
-    //
-//    // is the initial board solvable?
-    public boolean isSolvable() {
-        return true;
+        addNeighborTopq(b, pq, predecessor);
+        predecessor = b;
+        return false;
     }
 
-    //    // min number of moves to solve initial board; -1 if unsolvable
+    private void addNeighborTopq(Board b, MinPQ<Board> pq, Board predecessor) {
+        for (Board neighbor : b.neighbors()) {
+            if (neighbor.equals(predecessor)) {
+                continue;
+            }
+            pq.insert(neighbor);
+        }
+    }
+
+
+    public boolean isSolvable() {
+        return solvable;
+    }
+
     public int moves() {
         return moves;
     }
 
-    //
-//    // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
-        return queueOriginal;
+        if (isSolvable()) {
+            return sol;
+        }
+        return null;
     }
-//
-//    // solve a slider puzzle (given below)
-//    public static void main(String[] args) {
-//
-//    }
+
+    public static void main(String[] args) {
+
+    }
+
+    private class comparatorBoard implements Comparator<Board> {
+
+        @Override
+        public int compare(Board o1, Board o2) {
+            Integer o1Priority = o1.dimension() + o1.manhattan();
+            Integer o2Priority = o2.dimension() + o2.manhattan();
+            return o1Priority.compareTo(o2Priority);
+        }
+    }
 }
